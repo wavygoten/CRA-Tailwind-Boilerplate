506a59c29cd702363424f9ba78d0821448363c22
wavygoten <bossmanzach007@gmail.com>
2022-01-20 00:25:12 -0600

    fixed error

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,10 @@
 import tw from "twin.macro";
-const Input = tw.input`
-	bg-white focus:outline-none border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal m-10
+import styled from "styled-components";
+interface Props {
+	primary?: boolean;
+}
+const Input = styled.input<Props>`
+	${tw`bg-white focus:outline-none border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal m-10`}
+	background ${(props) => (props.primary ? "black" : "transparent")}
 `;
 export default Input;
